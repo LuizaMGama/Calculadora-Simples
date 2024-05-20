cbd2658e9b7f3810d201c824de51f7d123710c2a
LuizaMGama <luizamgama11@gmail.com>
2024-05-20 14:50:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Calculadora from './componentes/Calculadora/Calculadora'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Calculadora/>
+    </div>
+  )
+}
